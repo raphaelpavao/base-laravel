f83ce20f2351ed86df9eea69f45864b7e4a353c5
raphaelpavao <raphaelpavao@gmail.com>
2022-10-25 23:45:05 -0300

    Colocada middleware para validar acesso nas rotas web

--- routes/auth.php
+++ routes/auth.php
@@ -12,6 +12,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
+    Route::get('/', function(){
+        return redirect()->route('login');
+    });
+
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('auth/login');
+Route::middleware('guest')->group(function () {
+
+
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
+
+
+
+
 
 require __DIR__.'/auth.php';
